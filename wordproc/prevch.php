9610528afa10df60d411aa4fa4c1de627768847e
vcsmutl1528 <58227567+vcsmutl1528@users.noreply.github.com>
2022-07-06 20:55:24 +0300

    число букв от предыдущего слова
    подсчет числа букв, подходящих от предыдущего слова

--- wordproc/prevch.php
+++ wordproc/prevch.php
@@ -47,9 +47,9 @@ echo "Word count: $wc\n";
 echo "Prev ch count: $pc\n";
 echo "Prev ch count (no 1st letter): {$pc1}\n";
 echo "Word length stat:\n";
-for ($i = 0; $i <= 15; $i++) echo "$i:{$st[$i]} ";
+for ($i = 2; $i <= 15; $i++) echo "$i:{$lst[$i]} ";
 echo "\n";
 echo "Prev ch length stat:\n";
-for ($i = 2; $i <= 15; $i++) echo "$i:{$lst[$i]} ";
+for ($i = 0; $i <= 15; $i++) echo "$i:{$st[$i]} ";
 echo "\n";
 ?>
